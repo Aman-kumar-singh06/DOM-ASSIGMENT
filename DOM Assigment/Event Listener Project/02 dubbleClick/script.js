bc05d91a0ac5fd02af2c0519bb0a2ffc41ceb87c
Aman Kumar Singh <140542381+Aman-kumar-singh06@users.noreply.github.com>
2024-07-12 08:32:50 +0530

    Add files via upload

---
+++ DOM Assigment/Event Listener Project/02 dubbleClick/script.js
@@ -0,0 +1,9 @@
+//  dbclick event
+
+
+const boxElement = document.getElementById("box");
+
+// adding a dblclick event listener to the element
+boxElement.addEventListener("dblclick", function () {
+  alert("doubleClick event is working properly 👍");
+});
